e7bfc47ce82344becc9afec727da05da4379d882
LucianFE <lucianemanuel.f@gmail.com>
2025-01-08 13:23:56 +0200

    pakage.json updated for deploy

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const { appliedFilters } = useGlobalContext();
   return (
     <main className="min-h-screen bg-Light-Grayish-Cyan-(Background)">
-      <div className="h-[155px] bg-Desaturated-Dark-Cyan bg-[url('./images/bg-header-mobile.svg')] bg-cover bg-center md:bg-[url('./images/bg-header-desktop.svg')] relative"></div>
+      <div className="h-[155px] bg-Desaturated-Dark-Cyan bg-[url('images/bg-header-mobile.svg')] bg-cover bg-center md:bg-[url('images/bg-header-desktop.svg')] relative"></div>
       {appliedFilters.length > 0 && <TopFilters />}
 
       <JobsList />
